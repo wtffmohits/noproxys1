b482fc04c0c4bcf4d36b550fbcfc64a7a8781b67
Mohit Singh <116932546+wtffmohits@users.noreply.github.com>
2025-09-07 21:29:33 +0530

    day 141

--- lib/components/App_widgets/teachers/Buttons/button.dart
+++ lib/components/App_widgets/teachers/Buttons/button.dart
@@ -12,7 +12,7 @@ class BlueButton extends StatelessWidget {
     required this.lable,
     this.onPressed,
     this.onTap,
-    required String label,
+    required String label, required Text child,
   });
 
   @override
